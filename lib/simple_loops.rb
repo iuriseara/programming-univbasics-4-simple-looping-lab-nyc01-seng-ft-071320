1df2b297d5c40054f2e511c0bf3dca1aca9692ce
fearless-command-2358 <iuriseara@gmail.com>
2020-06-24 16:18:49 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,20 @@
 # Write your methods here
+def loop_message_five_times(parameter)
+  count = 0
+  while count < 5
+    puts parameter
+    count += 1
+  end
+end
+
+def loop_message_n_times
+
+end
+
+def output_array
+
+end
+
+def return_string_array
+
+end
